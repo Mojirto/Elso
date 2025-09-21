441533bba877d8ad6ef6fe2ed346fbe05521cb9d
Mojirto <bence.szucs92@gmail.com>
2025-09-22 01:01:59 +0200

    Kész

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Ez az első módosítás
 
+# Szűcs Bence FCYOFY
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
